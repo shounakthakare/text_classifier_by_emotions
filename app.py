611fa3cf2ed037a68474f21ba746978c2ebc5337
shounakthakare <shounak2410@gmail.com>
2026-02-12 10:40:20 +0530

    Initial Commit

--- app.py
+++ app.py
@@ -11,7 +11,15 @@ from track_utils import (
     IST
 )
 
-pipe_lr = joblib.load(open("./models/emotion_classifier_pipe_lr.pkl", "rb"))
+@st.cache_resource
+def load_model():
+    return joblib.load("./models/emotion_classifier_pipe_lr.pkl")
+
+@st.cache_resource
+def init_db():
+    create_emotionclf_table()
+
+pipe_lr = load_model()
 
 def predict_emotions(text):
     return pipe_lr.predict([text])[0]
@@ -33,12 +41,20 @@ emotions_emoji_dict = {
 }
 
 def emotion_app():
+    hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+.st-emotion-cache-scp8yw {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+"""
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
     st.title("🧠 Emotion Detection System")
 
-    menu = ["Home", "History", "Logout"]
+    menu = ["Home", "History"]
     choice = st.sidebar.selectbox("Menu", menu)
 
-    create_emotionclf_table()
+    init_db()
 
     if choice == "Home":
         st.subheader("Emotion Detection from Text")
